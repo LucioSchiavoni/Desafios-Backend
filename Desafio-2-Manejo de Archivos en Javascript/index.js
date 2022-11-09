101a6da00a73edc4591236ad6860f3869090461f
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2022-11-08 22:44:36 -0300

    Desafio-2

---
+++ Desafio-2-Manejo de Archivos en Javascript/index.js
@@ -0,0 +1,69 @@
+let fs = require("fs");
+
+
+class Contenedor {
+    constructor(file) {
+        this.file = file;
+
+    }
+
+    writeFile = async data => {
+        try {
+            await fs.promises.writeFile(
+                this.file, JSON.stringify(data, null, 2)
+            )
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    getAll = async () => {
+        try {
+            const productos = await fs.promises.readFile(this.file, 'utf-8');
+            return JSON.parse(productos);
+        } catch (err) {
+            if (err.message.includes('no such file or directory')) return [];
+            console.log("error");
+        }
+    }
+
+    save = async obj => {
+        let productos = this.getAll();
+        try {
+            let newId;
+            productos.length === 0 ? newId = 1 : newId = productos[productos.length - 1].id + 1;
+            let newObj = { ...obj, id: newId };
+            productos.push(newObj);
+            await this.writeFile(productos);
+            return newObj.id;
+        } catch (err) {
+            console.log("error");
+        }
+    }
+
+    getById = async id => {
+        let productos = await this.getAll();
+        try {
+            productos = productos.filter(producto => producto.id != id);
+            await this.writeFile(productos);
+        } catch (error) {
+            console.log("error");
+        }
+    }
+
+    deleteById = async id => {
+        let productos = await this.getAll();
+        try {
+            productos = productos.filter(producto => producto.id != id);
+            await this.writeFile(productos);
+        } catch (error) {
+            console.log("error");
+        }
+    }
+
+    deletAll = async () => {
+        this.writeFile([]);
+    }
+}
+
+module.exports = Contenedor;
---
+++ Desafio-2-Manejo de Archivos en Javascript/test.js
@@ -0,0 +1,24 @@
+const Contenedor = require('./index');
+
+const products = new Contenedor('productos.txt');
+
+const test = async () => {
+    let save = await products.save({
+        title: 'Havanna Semilla',
+        price: 40,
+        thumbail: "https://storage.lacapitalmdp.com/2019/12/semilia-1024x744.jpg"
+
+    });
+    let getAll = await products.getAll();
+    let getById = await products.getById(5);
+    let deleteById = await products.deleteById(2);
+    let deletAll = await products.deletAll();
+
+    console.log(save)
+    console.log(getAll)
+    console.log(getById)
+    console.log(deleteById)
+    console.log(deletAll)
+};
+
+test();
